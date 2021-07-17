2431e1b450d97b44f18b90e2cd477482d042af7c
keshavtakatrao <takatraokeshav@gmail.com>
2021-07-17 12:52:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  var axios
+  let [cardData, setCardData] = useState({});
+  let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM2MzVkNTc0LTQ2ZGItNGZkNy04NzUwLTEyNWM3MjNiNzYwMSIsImlhdCI6MTYyNjQ5OTUyOCwic3ViIjoiZGV2ZWxvcGVyL2UzMTIzZmQ5LTE2M2MtMjQzNy0wZmI3LWU2MzdmMjE0MzU1MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMDMuMTk5LjE3NS4xMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.UGvV2Ochkg0s8TxFwG1NVo_Zm4HkgcfWxMoyl0YMEpyJq7uIu70vGsdOAhE8pgk6L3afv_1_Nx9pVGVOhcT0XA"
+  useEffect(async () => {
+
+    var config = {
+      method: 'get',
+      url: 'https://api.clashroyale.com/v1/cards',
+      headers: {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM2MzVkNTc0LTQ2ZGItNGZkNy04NzUwLTEyNWM3MjNiNzYwMSIsImlhdCI6MTYyNjQ5OTUyOCwic3ViIjoiZGV2ZWxvcGVyL2UzMTIzZmQ5LTE2M2MtMjQzNy0wZmI3LWU2MzdmMjE0MzU1MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMDMuMTk5LjE3NS4xMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.UGvV2Ochkg0s8TxFwG1NVo_Zm4HkgcfWxMoyl0YMEpyJq7uIu70vGsdOAhE8pgk6L3afv_1_Nx9pVGVOhcT0XA'
+      }
+    };
+
+    await axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
